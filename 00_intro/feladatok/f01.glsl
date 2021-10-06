cfaba001324ad339e0f125300267af31bcc6b4d5
Erben Péter <erben.peter@gmail.com>
2021-10-06 17:51:55 +0200

    1.1 feladatok

---
+++ 00_intro/feladatok/f01.glsl
@@ -0,0 +1,20 @@
+const vec3 fcol = vec3(0., .8, 0.); // green
+const float PRECISION = 0.005;
+
+float f (float x)
+{
+  return x;
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+  vec2 uv = fragCoord/iResolution.xy;
+
+  float v = f(uv.x);
+  vec3 col = vec3(v);    
+  if (abs(v-uv.y) < PRECISION)
+  {
+    col = mix(col, fcol, .9);
+  }
+  fragColor = vec4(col,1.0);
+}
